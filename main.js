f5bb3481f0a0c6f021b8d92e98a3fb6dbe729083
unknown <gormkrtchyan02@gmail.com>
2020-11-10 00:09:27 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const util = require('./utils');
+
+let matrix1 = [[1,2],[3,4]];
+let matrix2 = [[5,6],[7,8]];
+
+
+console.log(util.matrixProduct(matrix1, matrix2));
+
+console.log(util.sumOfMatrices(matrix1, matrix2));
+
+console.log(util.sumOfEachRow(matrix1));
